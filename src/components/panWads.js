e0c0c2379229338a2c2f38bdad3de77ef5a7bc16
Austinmc41 <austinmc41@gmail.com>
2021-11-15 17:00:00 -0500

    pushing import fixes

--- src/components/panWads.js
+++ src/components/panWads.js
@@ -1,13 +1,50 @@
 import Wad from 'web-audio-daw';
-import sound1_raw from '../sounds/AirplaneJet 2004_38.mp3'
+import sound_AirplaneJet from '../sounds/AirplaneJet 2004_38.mp3'
+import sound_Baby from '../sounds/Baby 2012_49.mp3'
+import sound_BathtubDrain  from '../sounds/BathtubDrain TE035401.mp3'
+import sound_BellChurch  from '../sounds/BellChurch 2007_06.mp3'
+import sound_BubblesJacuzziCaul  from '../sounds/BubblesJacuzziCaul TE019301.mp3'
+import sound_Camera from '../sounds/Camera 2007_37.mp3'
+import sound_CarShortSlowSpeedS from '../sounds/CarShortSlowSpeedS TE046603.mp3'
+import sound_Chicken from '../sounds/Chicken 2004_63.mp3'
+import sound_CrowCawing  from '../sounds/CrowCawing TE013001.mp3'
+import sound_DogGrowlsViciously  from '../sounds/DogGrowlsViciously TE015601.mp3'
+import sound_FireFireplace  from '../sounds/FireFireplace 2010_36.mp3'
+import sound_FireworksFireworks  from '../sounds/FireworksFireworks TE028001.mp3'
+import sound_FlyBuzzesAgainstWi  from '../sounds/FlyBuzzesAgainstWi TE016601.mp3'
+import sound_FootstepsGiantTake  from '../sounds/FootstepsGiantTake TE028601.mp3'
+import sound_FootstepsGravel  from '../sounds/FootstepsGravel 2011_14_2.mp3'
+import sound_FootstepsLeaves  from '../sounds/FootstepsLeaves 2011_19_2.mp3'
+import sound_FootstepsMetal  from '../sounds/FootstepsMetal 2011_25_2.mp3'
+import sound_FootstepsSand  from '../sounds/FootstepsSand 2011_29_1.mp3'
+import sound_FootstepsSnow  from '../sounds/FootstepsSnow 2012_02_1.mp3'
+import sound_FootstepsStone  from '../sounds/FootstepsStone 2011_02_2.mp3'
+import sound_FootstepsWater  from '../sounds/FootstepsWater 2012_03_3.mp3'
+import sound_FootstepsWood from '../sounds/FootstepsWood 2012_04_2.mp3'
+import sound_Garbage  from '../sounds/Garbage 2010_66.mp3'
+import sound_KnifeSharpen  from '../sounds/KnifeSharpen 2014_47.mp3'
+import sound_LionGrowlsSnarls  from '../sounds/LionGrowlsSnarls TE016701.mp3'
+import sound_NutCrackers  from '../sounds/NutCrackers 2014_54.mp3'
+import sound_PlasticZipperClosi  from '../sounds/PlasticZipperClosi TE034603.mp3'
+import sound_SprayCan  from '../sounds/SprayCan 2014_60.mp3'
+import sound_SprinklerAutomatic  from '../sounds/SprinklerAutomatic TE049101.mp3'
+import sound_StadiumCrowdCheers  from '../sounds/StadiumCrowdCheers TE017302.mp3'
+import sound_TelephoneEuropean  from '../sounds/TelephoneEuropean 2003_06.mp3'
+import sound_Thunder_Boom_Fienup_005 from '../sounds/Thunder_Boom_Fienup_005.mp3'
+import sound_Toilet  from '../sounds/Toilet 2014_68.mp3'
+import sound_Waterfall1MediumSi  from '../sounds/Waterfall1MediumSi TE048701.mp3'
+import sound_saw4 from '../sounds/saw4.mp3'
+
 
 // from https://stackoverflow.com/questions/4602141/variable-name-as-a-string-in-javascript
 const varToString = varObj => Object.keys(varObj)[0]
 
-let sounds = []; 
+// sounds.array.forEach(element => {
+//     console.log(element);
+// });
 
 const sound1 = new Wad({
-    source: sound1_raw
+    source: sound_AirplaneJet
 })
 
 export function playSounds(){
--- src/object_sounds.js
+++ src/object_sounds.js
@@ -1,78 +1,78 @@
 
 //imports
-import sound_AirplaneJet 2004_38 from './sound/AirplaneJet 2004_38.mp3'
-import sound_Baby 2012_49 from './sound/Baby 2012_49.mp3'
-import sound_BathtubDrain TE035401 from './sound/BathtubDrain TE035401.mp3'
-import sound_BellChurch 2007_06 from './sound/BellChurch 2007_06.mp3'
-import sound_BubblesJacuzziCaul TE019301 from './sound/BubblesJacuzziCaul TE019301.mp3'
-import sound_Camera 2007_37 from './sound/Camera 2007_37.mp3'
-import sound_CarShortSlowSpeedS TE046603 from './sound/CarShortSlowSpeedS TE046603.mp3'
-import sound_Chicken 2004_63 from './sound/Chicken 2004_63.mp3'
-import sound_CrowCawing TE013001 from './sound/CrowCawing TE013001.mp3'
-import sound_DogGrowlsViciously TE015601 from './sound/DogGrowlsViciously TE015601.mp3'
-import sound_FireFireplace 2010_36 from './sound/FireFireplace 2010_36.mp3'
-import sound_FireworksFireworks TE028001 from './sound/FireworksFireworks TE028001.mp3'
-import sound_FlyBuzzesAgainstWi TE016601 from './sound/FlyBuzzesAgainstWi TE016601.mp3'
-import sound_FootstepsGiantTake TE028601 from './sound/FootstepsGiantTake TE028601.mp3'
-import sound_FootstepsGravel 2011_14_2 from './sound/FootstepsGravel 2011_14_2.mp3'
-import sound_FootstepsLeaves 2011_19_2 from './sound/FootstepsLeaves 2011_19_2.mp3'
-import sound_FootstepsMetal 2011_25_2 from './sound/FootstepsMetal 2011_25_2.mp3'
-import sound_FootstepsSand 2011_29_1 from './sound/FootstepsSand 2011_29_1.mp3'
-import sound_FootstepsSnow 2012_02_1 from './sound/FootstepsSnow 2012_02_1.mp3'
-import sound_FootstepsStone 2011_02_2 from './sound/FootstepsStone 2011_02_2.mp3'
-import sound_FootstepsWater 2012_03_3 from './sound/FootstepsWater 2012_03_3.mp3'
-import sound_FootstepsWood 2012_04_2 from './sound/FootstepsWood 2012_04_2.mp3'
-import sound_Garbage 2010_66 from './sound/Garbage 2010_66.mp3'
-import sound_KnifeSharpen 2014_47 from './sound/KnifeSharpen 2014_47.mp3'
-import sound_LionGrowlsSnarls TE016701 from './sound/LionGrowlsSnarls TE016701.mp3'
-import sound_NutCrackers 2014_54 from './sound/NutCrackers 2014_54.mp3'
-import sound_PlasticZipperClosi TE034603 from './sound/PlasticZipperClosi TE034603.mp3'
-import sound_SprayCan 2014_60 from './sound/SprayCan 2014_60.mp3'
-import sound_SprinklerAutomatic TE049101 from './sound/SprinklerAutomatic TE049101.mp3'
-import sound_StadiumCrowdCheers TE017302 from './sound/StadiumCrowdCheers TE017302.mp3'
-import sound_TelephoneEuropean 2003_06 from './sound/TelephoneEuropean 2003_06.mp3'
-import sound_Thunder_Boom_Fienup_005 from './sound/Thunder_Boom_Fienup_005.mp3'
-import sound_Toilet 2014_68 from './sound/Toilet 2014_68.mp3'
-import sound_Waterfall1MediumSi TE048701 from './sound/Waterfall1MediumSi TE048701.mp3'
-import sound_saw4 from './sound/saw4.mp3'
+import sound_AirplaneJet from './sounds/AirplaneJet 2004_38.mp3'
+import sound_Baby from './sounds/Baby 2012_49.mp3'
+import sound_BathtubDrain  from './sounds/BathtubDrain TE035401.mp3'
+import sound_BellChurch  from './sounds/BellChurch 2007_06.mp3'
+import sound_BubblesJacuzziCaul  from './sounds/BubblesJacuzziCaul TE019301.mp3'
+import sound_Camera from './sounds/Camera 2007_37.mp3'
+import sound_CarShortSlowSpeedS from './sounds/CarShortSlowSpeedS TE046603.mp3'
+import sound_Chicken from './sounds/Chicken 2004_63.mp3'
+import sound_CrowCawing  from './sounds/CrowCawing TE013001.mp3'
+import sound_DogGrowlsViciously  from './sounds/DogGrowlsViciously TE015601.mp3'
+import sound_FireFireplace  from './sounds/FireFireplace 2010_36.mp3'
+import sound_FireworksFireworks  from './sounds/FireworksFireworks TE028001.mp3'
+import sound_FlyBuzzesAgainstWi  from './sounds/FlyBuzzesAgainstWi TE016601.mp3'
+import sound_FootstepsGiantTake  from './sounds/FootstepsGiantTake TE028601.mp3'
+import sound_FootstepsGravel  from './sounds/FootstepsGravel 2011_14_2.mp3'
+import sound_FootstepsLeaves  from './sounds/FootstepsLeaves 2011_19_2.mp3'
+import sound_FootstepsMetal  from './sounds/FootstepsMetal 2011_25_2.mp3'
+import sound_FootstepsSand  from './sounds/FootstepsSand 2011_29_1.mp3'
+import sound_FootstepsSnow  from './sounds/FootstepsSnow 2012_02_1.mp3'
+import sound_FootstepsStone  from './sounds/FootstepsStone 2011_02_2.mp3'
+import sound_FootstepsWater  from './sounds/FootstepsWater 2012_03_3.mp3'
+import sound_FootstepsWood from './sounds/FootstepsWood 2012_04_2.mp3'
+import sound_Garbage  from './sounds/Garbage 2010_66.mp3'
+import sound_KnifeSharpen  from './sounds/KnifeSharpen 2014_47.mp3'
+import sound_LionGrowlsSnarls  from './sounds/LionGrowlsSnarls TE016701.mp3'
+import sound_NutCrackers  from './sounds/NutCrackers 2014_54.mp3'
+import sound_PlasticZipperClosi  from './sounds/PlasticZipperClosi TE034603.mp3'
+import sound_SprayCan  from './sounds/SprayCan 2014_60.mp3'
+import sound_SprinklerAutomatic  from './sounds/SprinklerAutomatic TE049101.mp3'
+import sound_StadiumCrowdCheers  from './sounds/StadiumCrowdCheers TE017302.mp3'
+import sound_TelephoneEuropean  from './sounds/TelephoneEuropean 2003_06.mp3'
+import sound_Thunder_Boom_Fienup_005 from './sounds/Thunder_Boom_Fienup_005.mp3'
+import sound_Toilet  from './sounds/Toilet 2014_68.mp3'
+import sound_Waterfall1MediumSi  from './sounds/Waterfall1MediumSi TE048701.mp3'
+import sound_saw4 from './sounds/saw4.mp3'
 
 
 //creating the objects
 const sounds = {
-	'AirplaneJet 2004_38': sounds_AirplaneJet 2004_38,
-	'Baby 2012_49': sounds_Baby 2012_49,
-	'BathtubDrain TE035401': sounds_BathtubDrain TE035401,
-	'BellChurch 2007_06': sounds_BellChurch 2007_06,
-	'BubblesJacuzziCaul TE019301': sounds_BubblesJacuzziCaul TE019301,
-	'Camera 2007_37': sounds_Camera 2007_37,
-	'CarShortSlowSpeedS TE046603': sounds_CarShortSlowSpeedS TE046603,
-	'Chicken 2004_63': sounds_Chicken 2004_63,
-	'CrowCawing TE013001': sounds_CrowCawing TE013001,
-	'DogGrowlsViciously TE015601': sounds_DogGrowlsViciously TE015601,
-	'FireFireplace 2010_36': sounds_FireFireplace 2010_36,
-	'FireworksFireworks TE028001': sounds_FireworksFireworks TE028001,
-	'FlyBuzzesAgainstWi TE016601': sounds_FlyBuzzesAgainstWi TE016601,
-	'FootstepsGiantTake TE028601': sounds_FootstepsGiantTake TE028601,
-	'FootstepsGravel 2011_14_2': sounds_FootstepsGravel 2011_14_2,
-	'FootstepsLeaves 2011_19_2': sounds_FootstepsLeaves 2011_19_2,
-	'FootstepsMetal 2011_25_2': sounds_FootstepsMetal 2011_25_2,
-	'FootstepsSand 2011_29_1': sounds_FootstepsSand 2011_29_1,
-	'FootstepsSnow 2012_02_1': sounds_FootstepsSnow 2012_02_1,
-	'FootstepsStone 2011_02_2': sounds_FootstepsStone 2011_02_2,
-	'FootstepsWater 2012_03_3': sounds_FootstepsWater 2012_03_3,
-	'FootstepsWood 2012_04_2': sounds_FootstepsWood 2012_04_2,
-	'Garbage 2010_66': sounds_Garbage 2010_66,
-	'KnifeSharpen 2014_47': sounds_KnifeSharpen 2014_47,
-	'LionGrowlsSnarls TE016701': sounds_LionGrowlsSnarls TE016701,
-	'NutCrackers 2014_54': sounds_NutCrackers 2014_54,
-	'PlasticZipperClosi TE034603': sounds_PlasticZipperClosi TE034603,
-	'SprayCan 2014_60': sounds_SprayCan 2014_60,
-	'SprinklerAutomatic TE049101': sounds_SprinklerAutomatic TE049101,
-	'StadiumCrowdCheers TE017302': sounds_StadiumCrowdCheers TE017302,
-	'TelephoneEuropean 2003_06': sounds_TelephoneEuropean 2003_06,
+	'AirplaneJet 2004_38': sounds_AirplaneJet,
+	'Baby 2012_49': sounds_Baby,
+	'BathtubDrain TE035401': sounds_BathtubDrain,
+	'BellChurch 2007_06': sounds_BellChurch,
+	'BubblesJacuzziCaul TE019301': sounds_BubblesJacuzziCaul,
+	'Camera 2007_37': sounds_Camera,
+	'CarShortSlowSpeedS TE046603': sounds_CarShortSlowSpeedS,
+	'Chicken 2004_63': sounds_Chicken,
+	'CrowCawing TE013001': sounds_CrowCawing,
+	'DogGrowlsViciously TE015601': sounds_DogGrowlsViciously,
+	'FireFireplace 2010_36': sounds_FireFireplace,
+	'FireworksFireworks TE028001': sounds_FireworksFireworks,
+	'FlyBuzzesAgainstWi TE016601': sounds_FlyBuzzesAgainstWi,
+	'FootstepsGiantTake TE028601': sounds_FootstepsGiantTake,
+	'FootstepsGravel 2011_14_2': sounds_FootstepsGravel,
+	'FootstepsLeaves 2011_19_2': sounds_FootstepsLeaves,
+	'FootstepsMetal 2011_25_2': sounds_FootstepsMetal,
+	'FootstepsSand 2011_29_1': sounds_FootstepsSand,
+	'FootstepsSnow 2012_02_1': sounds_FootstepsSnow,
+	'FootstepsStone 2011_02_2': sounds_FootstepsStone,
+	'FootstepsWater 2012_03_3': sounds_FootstepsWater,
+	'FootstepsWood 2012_04_2': sounds_FootstepsWood,
+	'Garbage 2010_66': sounds_Garbage,
+	'KnifeSharpen 2014_47': sounds_KnifeSharpen,
+	'LionGrowlsSnarls TE016701': sounds_LionGrowlsSnarls,
+	'NutCrackers 2014_54': sounds_NutCrackers,
+	'PlasticZipperClosi TE034603': sounds_PlasticZipperClosi,
+	'SprayCan 2014_60': sounds_SprayCan,
+	'SprinklerAutomatic TE049101': sounds_SprinklerAutomatic,
+	'StadiumCrowdCheers TE017302': sounds_StadiumCrowdCheers ,
+	'TelephoneEuropean 2003_06': sounds_TelephoneEuropean,
 	'Thunder_Boom_Fienup_005': sounds_Thunder_Boom_Fienup_005,
-	'Toilet 2014_68': sounds_Toilet 2014_68,
-	'Waterfall1MediumSi TE048701': sounds_Waterfall1MediumSi TE048701,
+	'Toilet 2014_68': sounds_Toilet,
+	'Waterfall1MediumSi TE048701': sounds_Waterfall1MediumSi ,
 	'saw4': sounds_saw4,
 }
 
