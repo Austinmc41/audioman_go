c41ae44e223f5a0adb5170b578cf28db38c62ad3
Austinmc41 <austinmc41@gmail.com>
2021-11-15 13:14:42 -0500

    creating button and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react"
+import panWads from './components/panWads'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className = 'container'><Header/>
     </div>
+  
   );
 }
 
--- src/components/panWads.js
+++ src/components/panWads.js
@@ -4,5 +4,4 @@ import Wad from 'web-audio-daw';
 let sounds = []; 
 
 
-
-
+// let sound = new Wad({source: });
