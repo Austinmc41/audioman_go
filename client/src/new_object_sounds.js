0c7d013913ce0d32e8504e02bca0d564e220a472
Brandon Biggs <brandonkeithbiggs@gmail.com>
2021-12-07 15:32:24 -0800

    added the new training_sounds

--- client/src/new_object_sounds.js
+++ client/src/new_object_sounds.js
@@ -1,34 +1,18 @@
 //sound imports
-import tairplane from './training_sounds/tairplane.mp3'
-import tbaby from './training_sounds/tbaby.mp3'
-import tbathtub from './training_sounds/tbathtub.mp3'
-import tbubbles from './training_sounds/tbubbles.mp3'
-import tcamera from './training_sounds/tcamera.mp3'
-import tcar from './training_sounds/tcar.mp3'
-import tchicken from './training_sounds/tchicken.mp3'
-import tCrowCawing from './training_sounds/tCrowCawing.mp3'
-import tfire from './training_sounds/tfire.mp3'
-import tfireworks from './training_sounds/tfireworks.mp3'
-import tfly from './training_sounds/tfly.mp3'
-import tsaw4 from './training_sounds/tsaw4.mp3'
-import zipper from './training_sounds/zipper.mp3'
+import glass_break from './training_sounds/glass_break.mp3'
+import guitar from './training_sounds/guitar.mp3'
+import piano from './training_sounds/piano.mp3'
+import violin from './training_sounds/violin.mp3'
+import walking from './training_sounds/walking.mp3'
 
 
 //creating the objects
 const sounds = {
-	'tairplane': tairplane,
-	'tbaby': tbaby,
-	'tbathtub': tbathtub,
-	'tbubbles': tbubbles,
-	'tcamera': tcamera,
-	'tcar': tcar,
-	'tchicken': tchicken,
-	'tCrowCawing': tCrowCawing,
-	'tfire': tfire,
-	'tfireworks': tfireworks,
-	'tfly': tfly,
-	'tsaw4': tsaw4,
-	'zipper': zipper,
+	'glass_break': glass_break,
+	'guitar': guitar,
+	'piano': piano,
+	'violin': violin,
+	'walking': walking,
 }
 
 export { sounds }
